67686fc26c97002d17a70f7f5936b22fa5d1a7d4
maopujie10 <maopujie10@gmail.com>
2026-05-30 11:21:58 +0800

    全量清理: 死代码删除13个API包装器+RAG chromadb安装脚本

--- frontend/src/api/inspector.js
+++
@@ -1,4 +0,0 @@
-import { agentApi } from './index'
-
-export function runInspection() { return agentApi.post('/inspector/run') }
-export function getInspectionHistory() { return agentApi.get('/inspector/history') }
